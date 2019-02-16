c70adb31d11fc2c02bd58eea694739ae455d764f
Angela <awebber1066@gmail.com>
2019-02-16 14:31:02 -0800

    Break Pots on attack

--- ProjectMythicalPrototype/Assets/Scripts/Environmental/BreakablebyPlayer.cs
+++ ProjectMythicalPrototype/Assets/Scripts/Environmental/BreakablebyPlayer.cs
@@ -13,6 +13,9 @@ public class BreakablebyPlayer : MonoBehaviour
     private Rigidbody _Rb;
     private AudioSource _Audio;
 
+    private bool _IsTouchingPlayer;
+    private bool _IsTouchingProjectile;
+
     private void Awake()
     {
         _Audio = GetComponent<AudioSource>();
@@ -22,9 +25,42 @@ public class BreakablebyPlayer : MonoBehaviour
     {
         if (other.CompareTag("Projectile"))
         {
-            _Audio.Play();
-            _OriginalObject.SetActive(false);
-            _BrokenObject.SetActive(true);
+            _IsTouchingProjectile = true;
+            Break();
         }
+        else
+        {
+            _IsTouchingProjectile = false;
+        }
+
+        if (other.CompareTag("Player"))
+        {
+            _IsTouchingPlayer = true;
+        }
+        else
+        {
+            _IsTouchingPlayer = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Projectile"))
+        {
+            _IsTouchingProjectile = false;
+        }
+
+        if (other.CompareTag("Player"))
+        {
+            _IsTouchingPlayer = false;
+        }
+    }
+
+    public void Break()
+    {
+        if (_IsTouchingProjectile || _IsTouchingPlayer)
+        _Audio.Play();
+        _OriginalObject.SetActive(false);
+        _BrokenObject.SetActive(true);
     }
 }

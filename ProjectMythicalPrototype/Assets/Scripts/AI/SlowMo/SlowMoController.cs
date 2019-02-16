3300d4c396ac9b49fe6ff9a3bf2ee14f2ef90872
Naani Surya Theja <snstheja@gmail.com>
2019-02-16 12:52:05 -0800

    Slow-Mo

--- ProjectMythicalPrototype/Assets/Scripts/AI/SlowMo/SlowMoController.cs
+++ ProjectMythicalPrototype/Assets/Scripts/AI/SlowMo/SlowMoController.cs
@@ -9,9 +9,13 @@ public class SlowMoController : MonoBehaviour
 
     [SerializeField]
     private float _SlowMoTime = 1f;
-
+    public static bool _SlowMoCD;
     [SerializeField]
     private float _SlowMoWait = 0.02f;
+    [SerializeField]
+    private float _SlowMoCoolDown = 4f;
+    private float _Delay=0;
+
 
     private bool _CanSlowMo = false;
 
@@ -29,9 +33,10 @@ public class SlowMoController : MonoBehaviour
 
     private void StartSlowMo()
     {
-        if (_CanSlowMo)
+        if (_CanSlowMo==true && _SlowMoCD==false)
         {
             Time.timeScale = _SlowMoAmount;
+            _SlowMoCD = true;
             StartCoroutine(ResetTimeScale());
         }
     }
@@ -41,5 +46,18 @@ public class SlowMoController : MonoBehaviour
         yield return new WaitForSecondsRealtime(_SlowMoTime);
         Time.timeScale = 1f;
         _CanSlowMo = false;
+    }
+    private void Update()
+    {
+        if(_SlowMoCD==true)
+        {
+            _Delay += Time.deltaTime;
+            if(_Delay>=_SlowMoCoolDown)
+            {
+                _Delay = 0;
+                _SlowMoCD = false;
+            }
+        }
+        
     }
 }

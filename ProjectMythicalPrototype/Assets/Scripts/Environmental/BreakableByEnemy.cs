e07bcba4db55543dd9454c18467b18d80dc8de1c
Naani Surya Theja <snstheja@gmail.com>
2019-02-16 11:02:56 -0800

    Deleted bugged bigpot ------ Flavio needs to replace

--- ProjectMythicalPrototype/Assets/Scripts/Environmental/BreakableByEnemy.cs
+++ ProjectMythicalPrototype/Assets/Scripts/Environmental/BreakableByEnemy.cs
@@ -10,10 +10,12 @@ public class BreakableByEnemy : MonoBehaviour
     private GameObject _BrokenObject;
     private Rigidbody _Rb;
     private AudioSource _Audio;
+    private bool _IsSoundPlayed;
     private void Awake()
     {
         _Rb = GetComponent<Rigidbody>();
         _Audio = GetComponent<AudioSource>();
+        _IsSoundPlayed = false;
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -21,8 +23,14 @@ public class BreakableByEnemy : MonoBehaviour
         {
             _OriginalObject.SetActive(false);
             _BrokenObject.SetActive(true);
+           
+            if(_IsSoundPlayed==false)
+            {
             _Audio.Play();
-            
+            }
         }
     }
 }
+
+            
+            

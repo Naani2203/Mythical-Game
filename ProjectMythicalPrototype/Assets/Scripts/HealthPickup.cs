b955f46234a632c0174f8d4266e6a5ef71eeb1ba
Naani Surya Theja <snstheja@gmail.com>
2019-02-16 17:47:54 -0800

    please do not change the audio source it is set up to work with flavios

--- ProjectMythicalPrototype/Assets/Scripts/HealthPickup.cs
+++ ProjectMythicalPrototype/Assets/Scripts/HealthPickup.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class HealthPickup : MonoBehaviour
 {
-    [SerializeField]
+   
     private AudioSource _Audio;
     [SerializeField]
     private AudioClip _HealthPickup;

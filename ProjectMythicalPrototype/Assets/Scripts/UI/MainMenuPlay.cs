2a90a543f5873c1dc6a66769e4baf11faf7b13e6
Naani Surya Theja <snstheja@gmail.com>
2019-02-18 00:18:55 -0800

    Main Menu Implemented

---
+++ ProjectMythicalPrototype/Assets/Scripts/UI/MainMenuPlay.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuPlay : MonoBehaviour
+{
+    [SerializeField]
+    private Animator _Anim;
+
+	public void Play()
+    {
+        _Anim.SetTrigger("ButtonPressed");
+    }
+
+    public void LoadGamePlayScene()
+    {
+        SceneManager.LoadScene("Flavio_Beta_Level");
+    }
+}

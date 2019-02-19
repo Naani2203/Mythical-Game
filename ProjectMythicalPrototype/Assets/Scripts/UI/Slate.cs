88e61d1af3b812b48caf87209a61e3fd87be3032
Naani Surya Theja <snstheja@gmail.com>
2019-02-18 22:50:37 -0800

    VFS slate implemented

---
+++ ProjectMythicalPrototype/Assets/Scripts/UI/Slate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Slate : MonoBehaviour
+{
+    private float _Delay;
+    private bool _IsDelay;
+
+    private void Awake()
+    {
+        _IsDelay = true;
+        _Delay = 0;
+    }
+    private void Update()
+    {
+        _Delay += Time.deltaTime;
+        if(_Delay>=5.0f)
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+
+
+}
